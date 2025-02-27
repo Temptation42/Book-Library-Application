7ae3bc0d3784281222ef5e446350d856a33c9105
Temptation42 <omnilija@gmail.com>
2025-02-27 18:05:55 +0100

    Updated: urls

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ console.log("PORT is ", process.env.PORT)
 const __dirname = path.resolve();
 
 //middlewares
-app.use(cors({origin: "http://localhost:5173", credentials: true}));
+app.use(cors({origin: "https://book-library-application-b14g.onrender.com", credentials: true}));
 app.use(express.json({limit: "20mb"}));
 app.use(cookieParser());
 
--- frontend/src/store/bookStore.js
+++ frontend/src/store/bookStore.js
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://book-library-application-b14g.onrender.com/api";
 axios.defaults.withCredentials = true;
 
 export const useBookStore = create((set) => ({
